6bfdd896643d668c5d1badd24e1d573fd7b2229e
Herve Bourelle <herve.bourelle@gmail.com>
2019-12-24 17:43:08 +0100

    nav correction

---
+++ yodaconvert/src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+
+
+function Nav(props) {
+  return (
+    <div>
+    <div className = 'navbar' style={{marginTop:'60px'}}>
+      <div >
+        <h1><img src='logoMkdown.png' alt='logo' className='navbar_title'/></h1>
+      </div>
+    </div>
+    <div style={{display:'flex',alignItems:'center'}}>
+    <div className='search-field'>
+      <input onChange={props.searchField} value={props.input} id='searchField' type ='text' placeholder ='Search Key word'></input>
+    </div>
+    <div>
+  <p style={{color:'yellow',paddingLeft:'15px'}}>Key words : {props.onSearch()}<span style={{paddingLeft:'15px'}}>  Words : {props.counter}</span></p>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default Nav;
